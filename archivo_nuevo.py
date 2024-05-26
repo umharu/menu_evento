e5460efd6522e31ebdb45978f50ad5decb1e143c
MRoldanCapponi <maxiroldanc@gmail.com>
2024-05-25 23:30:23 -0300

    Lista eventos

--- archivo_nuevo.py
+++ archivo_nuevo.py
@@ -4,17 +4,17 @@ def registrar_partipantes(nombre, apellido, dni):
     if verificar_cupo():
         participante = (nombre, apellido, dni)
         evento[4].append(participante)
-        print(f"participante {nombre}{apellido}{dni}registrado correctamente")
+        print(f"Participante {nombre} {apellido} {dni} ha registrado correctamente")
     else:
         print("No hay cupos disponibles")
 
 def verificar_cupo():
-    return len(evento[4] < evento[3])
+    return len(evento[4]) < evento[3]  
 
 def listar_participantes():
     if evento[4]:
         for i , participante in enumerate(evento[4], start=1):
-            print(f"{i}.{participante[0]}{participante[1]} - documento: {participante[2]}")
+            print(f"{i}:{participante[0]} {participante[1]} - documento: {participante[2]}")
         else:
             print("No hay participantes registrados.")
 
@@ -25,20 +25,22 @@ def menu():
         print("2. Verificar cupo")
         print("3. Listar participantes")
         print("4. Salir")
-opcion = input("Ingrese una opcion: ")
-if opcion == "1":
-    nombre = input("Ingrese nombre: ")
-    apellido = input("Ingrese apellido: ")
-    dni = input("Ingrese dni: ")
-    registrar_partipantes(nombre, apellido, dni)
-elif opcion == "2":
-    if verificar_cupo():
-        print("Hay cupos")
-    else:
-        print("No hay cupos")
-elif opcion == "3":
-        listar_participantes()
-elif opcion == "4":
-        break
-else:
-        print("Opcion incorrecta, seleccione del 1 al 4. ")
+        opcion = input("Ingrese una opcion: ")
+        if opcion == "1":
+            nombre = input("Ingrese nombre: ")
+            apellido = input("Ingrese apellido: ")
+            dni = input("Ingrese dni: ")
+            registrar_partipantes(nombre, apellido, dni)
+        elif opcion == "2":
+            if verificar_cupo():
+                print("Hay cupos")
+            else:
+                print("No hay cupos")
+        elif opcion == "3":
+                listar_participantes()
+        elif opcion == "4":
+            break
+        else:
+            print("Opcion incorrecta, seleccione del 1 al 4. ")
+            
+nuevo_menu = menu()
